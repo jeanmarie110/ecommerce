37c2be46e7cf4294fe9980f48cd12523880ae92a
jwang0306 <ace020771@gmail.com>
2021-11-16 12:38:16 -0600

    Modify main page

--- frontend/src/containers/MainPage/MainPage.js
+++ frontend/src/containers/MainPage/MainPage.js
@@ -4,9 +4,8 @@ import { connect } from 'react-redux'
 const MainPage = (props) => {
     return (
         <div>
-            <h1>Main Page!</h1>
-            <h2>Welcome to ecommerce fun!</h2>
-            <h3>List some of the best selling products here...</h3>
+            <h1>Main Page</h1>
+            <h2>Welcome to ecommerce fullstack!</h2>
         </div>
     )
 }
